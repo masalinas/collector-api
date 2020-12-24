ef3741fe0ce8cec42ec338f0e71d675fdcebf234
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2020-12-24 21:20:22 +0100

    last update

--- src/application.ts
+++ src/application.ts
@@ -7,7 +7,9 @@ import {
 import {RepositoryMixin} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
+import multer from 'multer';
 import path from 'path';
+import {FILE_UPLOAD_SERVICE, STORAGE_DIRECTORY} from './keys';
 import {MySequence} from './sequence';
 
 export {ApplicationConfig};
@@ -30,6 +32,9 @@ export class CollectorApiApplication extends BootMixin(
     });
     this.component(RestExplorerComponent);
 
+    // Configure file upload with multer options
+    this.configureFileUpload(options.fileStorageDirectory);
+
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
     this.bootOptions = {
@@ -40,5 +45,25 @@ export class CollectorApiApplication extends BootMixin(
         nested: true,
       },
     };
+  }
+
+  /**
+   * Configure `multer` options for file upload
+   */
+  protected configureFileUpload(destination?: string) {
+    // Upload files to `dist/.sandbox` by default
+    destination = destination ?? path.join(__dirname, '../.sandbox');
+    this.bind(STORAGE_DIRECTORY).to(destination);
+    const multerOptions: multer.Options = {
+      storage: multer.diskStorage({
+        destination,
+        // Use the original file name as is
+        filename: (req, file, cb) => {
+          cb(null, file.originalname);
+        },
+      }),
+    };
+    // Configure the file upload service with multer options
+    this.configure(FILE_UPLOAD_SERVICE).to(multerOptions);
   }
 }
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,5 @@ export * from './piece-family.controller';
 export * from './piece-category.controller';
 export * from './piece-sub-category.controller';
 export * from './piece.controller';
+export * from './file-download.controller';
+export * from './file-upload.controller';
