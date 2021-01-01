6cf0dc015f2829b45be119fffe6f47f222af4547
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2021-01-01 12:34:39 +0100

    last update

---
+++ src/controllers/file.controller.ts
@@ -0,0 +1,146 @@
+// Copyright IBM Corp. 2020. All Rights Reserved.
+// Node module: @loopback/example-file-transfer
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {inject} from '@loopback/core';
+import {
+  get,
+  del,
+  HttpErrors,
+  oas,
+  param,
+  post,
+  Request,
+  requestBody,
+  Response,
+  RestBindings,
+} from '@loopback/rest';
+import fs from 'fs';
+import path from 'path';
+import {promisify} from 'util';
+import {STORAGE_DIRECTORY} from '../keys';
+import {FILE_UPLOAD_SERVICE} from '../keys';
+import {FileUploadHandler} from '../types';
+
+const readdir = promisify(fs.readdir);
+const delfile = promisify(fs.unlink);
+
+export class FileController {
+  /**
+   * Constructor
+   * @param handler - Inject an express request handler to deal with the request
+   */
+  constructor(@inject(FILE_UPLOAD_SERVICE) private handler: FileUploadHandler,
+              @inject(STORAGE_DIRECTORY) private storageDirectory: string) {}
+
+  /**
+   * Get files and fields for the request
+   * @param request - Http request
+   */
+  private static getFilesAndFields(request: Request) {
+    const uploadedFiles = request.files;
+    const mapper = (f: globalThis.Express.Multer.File) => ({
+      fieldname: f.fieldname,
+      originalname: f.originalname,
+      encoding: f.encoding,
+      mimetype: f.mimetype,
+      size: f.size,
+    });
+    let files: object[] = [];
+    if (Array.isArray(uploadedFiles)) {
+      files = uploadedFiles.map(mapper);
+    } else {
+      for (const filename in uploadedFiles) {
+        files.push(...uploadedFiles[filename].map(mapper));
+      }
+    }
+    return {files, fields: request.body};
+  }
+
+  /**
+   * Validate file names to prevent them goes beyond the designated directory
+   * @param fileName - File name
+   */
+  private validateFileName(fileName: string) {
+    const resolved = path.resolve(this.storageDirectory, fileName);
+    if (resolved.startsWith(this.storageDirectory)) return resolved;
+    // The resolved file is outside sandbox
+    throw new HttpErrors.BadRequest(`Invalid file name: ${fileName}`);
+  }
+
+  @post('/files', {
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+            },
+          },
+        },
+        description: 'Files and fields',
+      },
+    },
+  })
+  async fileUpload(
+    @requestBody.file() request: Request,
+    @inject(RestBindings.Http.RESPONSE) response: Response,
+  ): Promise<object> {
+    return new Promise<object>((resolve, reject) => {
+      this.handler(request, response, (err: unknown) => {
+        if (err) reject(err);
+        else {
+          resolve(FileController.getFilesAndFields(request));
+        }
+      });
+    });
+  }
+
+  @get('/files', {
+    responses: {
+      200: {
+        content: {
+          // string[]
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: {
+                type: 'string',
+              },
+            },
+          },
+        },
+        description: 'A list of files',
+      },
+    },
+  })
+  async listFiles() {
+    const files = await readdir(this.storageDirectory);
+    return files;
+  }
+
+  @get('/files/{filename}')
+  @oas.response.file()
+  downloadFile(
+    @param.path.string('filename') fileName: string,
+    @inject(RestBindings.Http.RESPONSE) response: Response,
+  ) {
+    const file = this.validateFileName(fileName);
+    response.download(file, fileName);
+    return response;
+  }
+
+  @del('/files/{filename}', {
+    responses: {
+      '204': {
+        description: 'File DELETE success',
+      },
+    },
+  })
+  async deleteByName(@param.path.string('filename') fileName: string): Promise<void> {
+    const file = this.validateFileName(fileName);
+
+    await delfile(file);
+  }
+}
